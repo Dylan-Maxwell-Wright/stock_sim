16e3561997b280360614e47b9c237eb52e47a162
Dylan Wright <dylan.wright03@gmail.com>
2019-10-25 12:28:32 -0700

    static files, bootstrap, crispyforms, html files added

--- stocksimulator/settings.py
+++ stocksimulator/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ['192.168.99.100',]
 
 INSTALLED_APPS = [
     'users.apps.UsersConfig',
+    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -123,3 +124,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'staticfiles'),
+]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
