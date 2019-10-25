d4f4036fabcfae87ddcf0443ce7ea09b2569a195
Dylan Wright <dylan.wright03@gmail.com>
2019-10-25 14:21:14 -0700

    moved user html template files, new portfolios app

--- stocksimulator/settings.py
+++ stocksimulator/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ['192.168.99.100',]
 
 INSTALLED_APPS = [
     'users.apps.UsersConfig',
+    'portfolios.apps.PortfoliosConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
